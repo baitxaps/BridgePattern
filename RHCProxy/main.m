2ffe9e571e45f19b385b17d5e4d3c7fc166c4175
rhc <bawfnhaps@163.com>
2016-07-16 15:45:43 +0800

    Initial Commit

---
+++ RHCProxy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RHCProxy
+//
+//  Created by hairong chen on 16/7/16.
+//  Copyright © 2016年 hairong chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
